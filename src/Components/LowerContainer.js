698477f55386e4046ade9952d3e24db7b2e63396
RebazMohsen <Rebazmohsen1111@gmail.com>
2022-07-05 14:57:15 +0300

    fix: console Error

--- src/Components/LowerContainer.js
+++ src/Components/LowerContainer.js
@@ -9,11 +9,12 @@ export default function LowerContainer() {
           width="24"
           height="24"
           viewBox="0 0 24 24"
-          stroke-width="2"
+          /*  stroke-width="2" */
+          strokeWidth={"2"}
           stroke="currentColor"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap={"round"}
+          strokeLinejoin={"round"}
         >
           {" "}
           <path stroke="none" d="M0 0h24v24H0z" />{" "}
@@ -34,9 +35,10 @@ export default function LowerContainer() {
           stroke="currentColor"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap={"round"}
+            strokeLinejoin={"round"}
+            /*  stroke-width="2" */
+            strokeWidth={"2"}
             d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
           />
         </svg>
@@ -55,15 +57,16 @@ export default function LowerContainer() {
           stroke="currentColor"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap={"round"}
+            strokeLinejoin={"round"}
+            /* stroke-width="2" */
+            strokeWidth={"2"}
             d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
           />
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap={"round"}
+            strokeLinejoin={"round"}
+            strokeWidth={"2"}
             d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
           />
         </svg>
